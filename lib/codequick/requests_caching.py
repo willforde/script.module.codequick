ddeed2efc699a7dffa34de979f524d4b9cc1a390
William Forde <willforde+github@gmail.com>
2016-03-21 18:29:19 +0000

    First Commit

---
+++ lib/codequick/requests_caching.py
@@ -0,0 +1,350 @@
+# Standard Library Imports
+import io
+import os
+import zlib
+import time
+import base64
+import hashlib
+
+# Requests Related Imports
+import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.response import HTTPResponse
+from requests.packages.urllib3 import disable_warnings
+from requests.structures import CaseInsensitiveDict
+
+# Package imports
+from .api import addonData, refresh
+from .logging import logger
+from . import json
+
+def _full_cache_dir(cache_dir):
+	""" Return cache directory with sub directory added """
+	return os.path.join(cache_dir, u"cache")
+
+def cache_session(cache_dir, max_age=0, max_retries=0):
+	"""
+	Create request session with support for http caching
+	
+	cache_dir : string or unicode --- Directory where to store the cache files
+	[max_age] : integer --- Max age that the cache can be before it becomes stale (default 0)
+	[max_retries] : integer --- Max amount of retries before request gives up and raises an error (default 0)
+	"""
+	
+	# Create session object
+	session = requests.session()
+	disable_warnings()
+	
+	# Add max age to header
+	session.headers["X-Max-Age"] = max_age
+	
+	# Create a HTTPAdapter Object to be used in requests
+	cache_dir = _full_cache_dir(cache_dir)
+	adapter = CacheAdapter(cache_dir, max_age, max_retries=max_retries)
+	
+	# Mount The new adapter to the current request session
+	session.mount("http://", adapter)
+	session.mount("https://", adapter)
+	return session
+
+
+class CacheAdapter(HTTPAdapter):
+	"""
+	Requests adapter used to intercept the request and responses from the requests module
+	
+	cache_dir : string or unicode --- Directory where to store the cache files
+	max_age : integer --- Max age that the cache can be before it becomes stale
+	*args, **kwargs --- Any or all keywords that are accepted by requests.adapters.HTTPAdapter
+	"""
+	
+	def __del__(self):
+		"""
+		Cleanup method to remove stale caches to save on disk space
+		Executes every 14 days (2 weeks)
+		"""
+		
+		# Fetch time of last cleanup operation
+		lastTime = addonData.getSetting("_lastcleanup")
+		try: lastTime = float(lastTime) + 1209600 # Every 14 Days
+		except ValueError: lastTime = 0
+		
+		# Cleanup if lasttime + 28 days is still less than current time
+		currentTime = time.time()
+		if lastTime < currentTime:
+			logger.debug("Initiating Cache Cleanup...")
+			
+			# Loop each file within urlcache folder
+			for hash in os.listdir(self._cache_dir):
+				# Load Cache and check if stale
+				cache = CacheHandler(self._cache_dir, hash, preload_content=True)
+				if cache.exists() and cache.fresh(60*24*14) is False:
+					logger.debug("Removing cache: %s", hash)
+					cache.delete()
+			
+			# Save currentTime of cleanup for later use
+			addonData.setSetting(u"_lastcleanup", str(currentTime))
+	
+	def __init__(self, cache_dir, max_age, *args, **kwargs):
+		# Call Parent init method
+		super(CacheAdapter, self).__init__(*args, **kwargs)
+		
+		# Set Instance Variables
+		self._cache_dir = cache_dir
+		self._max_age = max_age
+		self._cache = None
+	
+	def send(self, request, **kwargs):
+		"""
+		Use the request information to check if it exists in the cache
+		and return cached response if so. Else forward on the said request
+		"""
+		
+		logger.debug("%s Requesting Url %s", request.method, request.url)
+		max_age = 0 if refresh is True else int(request.headers.pop("X-Max-Age", self._max_age))
+		
+		# Cache only handle's get requests
+		if request.method == "GET":
+			# Initialize Cache Handler
+			hash = self._encode_url(request.url)
+			self._cache = cache = CacheHandler(self._cache_dir, hash, preload_content=True)
+			if cache.exists():
+				if cache.fresh(max_age):
+					# Build the request response and return
+					logger.debug("Cache is fresh, using cached response")
+					return self.build_response(request, cache.response, from_cache=True)
+				else:
+					# Set cache headers to allow for 304 Not Modified response
+					logger.debug("Cache is stale, setting conditional headers if any")
+					request.headers.update(cache.conditional_headers)
+		
+		# Forward on the request to be sent
+		kwargs["verify"] = False
+		before = time.time()
+		response = super(CacheAdapter, self).send(request, **kwargs)
+		logger.debug("HTTP Request took %s seconds to complete", time.time() - before)
+		return response
+	
+	def build_response(self, request, response, from_cache=False):
+		""" Build a requests response object """
+		
+		# Handle server responses
+		if from_cache is False and request.method == "GET":
+			# Create cache object if for some reason it was not already created
+			if self._cache is None:
+				hash = self._encode_url(request.url)
+				self._cache = CacheHandler(self._cache_dir, hash, preload_content=False)
+			
+			# Check for a 304 Not Modified Response and use cache if true
+			if response.status == 304:
+				logger.debug("Server return 304 Not Modified response, using cached response")
+				# Refresh the cache as it's still fresh
+				self._cache.update_cache()
+				
+				# Read a possible response body and release the connection.
+				response.read(decode_content=False)
+				response.release_conn()
+				
+				# Set response to cached response
+				response = self._cache.response
+			
+			# Always cache a 301 Moved Permanently response
+			elif response.status == 301:
+				# Cache the response
+				self._cache.cache_response(response)
+			
+			# Cache any cachable response
+			elif response.status in (200, 203, 300):
+				# Wrap the response file with a wrapper that will cache the
+				# response when the stream has been consumed.
+				logger.debug("Caching request response")
+				if response.chunked: body = "".join([chunk for chunk in response.stream(decode_content=False)])
+				else: body = response.read(decode_content=False)
+				self._cache.cache_response(response, body)
+				response = self._cache.response
+		
+		# Return requests response object
+		return super(CacheAdapter, self).build_response(request, response)
+	
+	@staticmethod
+	def _encode_url(url):
+		""" Return url as a sha1 encoded hash """
+		if "#" in url: url = url[:url.find("#")]
+		hash = hashlib.sha1(url).hexdigest()
+		return hash
+	
+	def close(self):
+		""" Reset instance variables """
+		super(CacheAdapter, self).close()
+		if self._cache is not None:
+			self._cache.close()
+			self._cache = None
+
+
+class CacheHandler(object):
+	"""
+	Cache Handler to handle the cache thats stored on disk
+	
+	cache_dir : string or unicode --- Directory where to store the cache files
+	hash : string or unicode --- sha256 hash of the url of the request to be cached
+	"""
+	
+	def __init__(self, cache_dir, hash, preload_content=True):
+		# Construct the full path to cache
+		logger.debug("Hash => %s", hash)
+		self._cache_path = os.path.join(cache_dir, hash)
+		self._response = None
+		
+		# Check if there is a cache for giving url
+		if not os.path.exists(self._cache_path):
+			logger.debug("Cache Not Found")
+			if not os.path.exists(cache_dir):
+				logger.debug("Creating Cache Directory")
+				os.makedirs(cache_dir)
+		
+		# Cache must exist then deserialize the cache
+		elif preload_content is True: self.load()
+	
+	def load(self):
+		""" Load the content of cache into memory """
+		self._response = self.deserialize()
+	
+	@staticmethod
+	def delete(path):
+		""" Delete cache on disk"""
+		try: os.remove(path)
+		except OSError: logger.debug("Cache Error: Unable to delete cache from disk")
+		self.close()
+	
+	@property
+	def response(self):
+		""" Return the cache response as a urllib3 HTTPResponse """
+		if "timestamp" in self._response: del self._response["timestamp"]
+		body = io.BytesIO(self._response.pop("body"))
+		return HTTPResponse(body=body, preload_content=False, **self._response)
+	
+	@property
+	def conditional_headers(self):
+		""" Return a dict of conditional headers from cache """
+		
+		# Fetch Cached headers
+		headers = CaseInsensitiveDict(self._response["headers"])
+		new_headers = {}
+		
+		# Check for conditional headers
+		if "etag" in headers: new_headers["If-None-Match"] = headers["etag"]
+		if "last-modified" in headers: new_headers["If-Modified-Since"] = headers["last-modified"]
+		return new_headers
+	
+	def deserialize(self):
+		""" Return a deserialize version of the cache from disk """
+		
+		try:
+			# Fetch raw cache data
+			with open(self._cache_path, "rb") as stream:
+				raw_data = stream.read()
+		except IOError, OSError:
+			logger.debug("Cache Error: Failed to read cache from disk")
+			self.delete(self._cache_path)
+			return None
+		else:
+			if not raw_data:
+				logger.debug("Cache Error: Cache was empty")
+				self.delete(self._cache_path)
+				return None
+		
+		try:
+			# Deserialize and decode the raw data
+			uncompressed = zlib.decompress(raw_data)
+			cached = json.loads(uncompressed)
+			cached["body"] = base64.b64decode(str(cached["body"]))
+			cached["headers"] = {key: unicode(base64.b64decode(str(value))) for key, value in cached["headers"].iteritems()}
+		except ValueError, TypeError:
+			logger.debug("Cache Error: Failed to deserialize the contents of the cache")
+			self.delete(self._cache_path)
+			return None
+		except zlib.error:
+			logger.debug("Cache Error: Failed to Decompress Stored Cache")
+			self.delete(self._cache_path)
+			return None
+		else:
+			return cached
+	
+	def exists(self):
+		""" Return True if cache exists else False """
+		return self._response is not None
+	
+	def fresh(self, max_age):
+		""" Return True if cache is fresh else False """
+		if max_age == 0: return False
+		else: return max_age < 0 or self._response["status"] == 301 or (time.time() - self._response.get("timestamp", 0)) < max_age * 60
+	
+	def update_cache(self, response=None):
+		"""
+		Serialize the response and save to disk
+		
+		[response] : dict --- Dict containging server response data (default None)
+		
+		NOTE
+		If no response is giving then the currently stored response is used
+		"""
+		
+		# Fetch response if not giving one
+		if response is None: response = self._response.copy()
+		
+		# Set the timestamp of the response
+		response["timestamp"] = time.time()
+		
+		# Serialize the response content to insure that json can do it's job
+		response["body"] = base64.b64encode(response["body"])
+		response["headers"] = {key:base64.b64encode(str(value)) for key, value in response["headers"].iteritems()}
+		
+		# Serialize the whole response
+		try: jsonSerial = json.dumps(response, ensure_ascii=True, indent=4, separators=(",", ":"))
+		except TypeError:
+			logger.debug("Cache Error: Failed to serialize the response using json")
+			return None
+		
+		# Compress the json Serialized response
+		try: compressed = zlib.compress(jsonSerial, 1)
+		except zlib.error:
+			logger.debug("Cache Error: Failed to compress serialized response")
+			return None
+		
+		# Save serialized response to disk
+		try: 
+			with open(self._cache_path, "wb") as stream:
+				stream.write(compressed)
+		except IOError, OSError:
+			logger.debug("Cache Error: Failed to Save serialized response to disk")
+			self.delete(self._cache_path)
+			return None
+	
+	def cache_response(self, response, body=None):
+		"""
+		Create data structure and Cache the response
+		
+		response : urllib3 response object --- A urllib3 response that was returned from the server
+		[body] : string --- Separated response body to use, uses body from response if body is not giving (default None)
+		"""
+		if body is None: body = response.read(decode_content=False)
+		headers = response.headers
+		
+		# Remove Transfer-Encoding from header if response was a chunked response
+		if "transfer-encoding" in headers: del headers["transfer-encoding"]
+		
+		# Create response data structure
+		data = {"body": body,
+				"headers": headers,
+				"status": response.status,
+				"version": response.version,
+				"reason": response.reason,
+				"strict": response.strict,
+				"decode_content": response.decode_content}
+		
+		# Update cache with response data
+		self._response = data.copy()
+		self.update_cache(data)
+	
+	def close(self):
+		""" Reset instance variables and loaded response"""
+		self._response = None
---
+++ lib/codequick/urllib_caching.py
@@ -0,0 +1,399 @@
+# Standard Library Imports
+import io
+import os
+import re
+import zlib
+import time
+import base64
+import urllib
+import urllib2
+import httplib
+import hashlib
+import urlparse
+import StringIO
+import functools
+import htmlentitydefs
+
+# Package imports
+from .api import addonData, refresh
+from .logging import logger
+from . import json
+
+def _full_cache_dir(cache_dir):
+	""" Return cache directory with sub directory added """
+	return os.path.join(cache_dir, u"cache-basic")
+
+def cache_session(cache_dir, max_age=0, max_retries=0):
+	"""
+	Create request session with support for http caching
+	
+	cache_dir : string or unicode --- Directory where to store the cache files
+	[max_age] : integer --- Max age that the cache can be before it becomes stale (default 0)
+	[max_retries] : integer --- Max amount of retries before request gives up and raises an error (default 0)
+	"""
+	
+	# Create session object
+	session = Session()
+	
+	# Add max age to header
+	session.headers["X-Max-Age"] = max_age
+	
+	# Create a HTTPAdapter Object to be used in requests
+	cache_dir = _full_cache_dir(cache_dir)
+	adapter = CacheAdapter(cache_dir, max_age, max_retries=max_retries)
+	
+	# Mount The new adapter to the current request session
+	session.mount(adapter)
+	return session
+
+
+class RequestRes(object):
+	def __init__(self, response):
+		self.status_code = response.getcode()
+		self.headers = headers = response.info()
+		self.reason = response.msg
+		content = response.read()
+		response.close()
+		self.encoding = None
+		self.__text = None
+		self.url = response.geturl()
+		
+		# Check if Response need to be decoded, else return raw response
+		charset = headers.getparam("charset") or headers.getparam("encoding")
+		if charset: self.encoding = charset
+		contentEncoding = headers.get(u"content-encoding")
+		
+		# If content is compressed then decompress and decode into unicode
+		try:
+			print "# len compressed", len(content)
+			if contentEncoding and "gzip" in contentEncoding: content = zlib.decompress(content, 16+zlib.MAX_WBITS)
+			elif contentEncoding and "deflate" in contentEncoding: content = zlib.decompress(content)
+		
+		except zlib.error as e:
+			print "error: %s" % e
+		
+		else:
+			print "# len uncompressed", len(content)
+			self.content = content
+	
+	@property
+	def text(self):
+		if self.__text: return self.__text
+		else:
+			# Convert html to unicode
+			try: unicodeData = unicode(self.content, self.encoding if self.encoding else "utf8")
+			except: unicodeData = unicode(self.content, "iso-8859-1")
+			
+			# Unescape the content if requested
+			return self._unescape(unicodeData)
+	
+	def _unescape(self, text):
+		# Add None Valid HTML Entities
+		htmlentitydefs.name2codepoint["apos"] = 0x0027
+		
+		def fixup(m):
+			# Fetch Text from Group
+			text = m.group(0)
+			# Check if Character is A Character Reference or Named Entity
+			if text[:2] == "&#": # Character Reference
+				try:
+					if text[:3] == "&#x": return unichr(int(text[3:-1], 16))
+					else: return unichr(int(text[2:-1]))
+				except ValueError: return text
+			else: # Named Entity
+				try: return unichr(htmlentitydefs.name2codepoint[text[1:-1]])
+				except KeyError: return text
+		
+		# Return Clean string using accepted encoding
+		return re.sub("&#?\w+;", fixup, text)
+	
+	def json(self, **kwargs):
+		return json.loads(self.content, encoding=self.encoding, **kwargs)
+	
+	@property
+	def elapsed(self):
+		return 0.0
+	
+	def close(self):
+		pass
+
+
+class Session(object):
+	def __init__(self):
+		self.handleList = []
+		self.headers = {}
+		self.__opener = None
+	
+	def mount(self, adapter):
+		self.handleList.append(adapter)
+	
+	def request(self, method, url, params=None, data=None, headers=None, timeout=None):
+		if params:
+			params = urllib.urlencode(params)
+			urlParts = urlparse.urlsplit(url)
+			url = urlparse.urlunsplit((urlParts.scheme, urlParts.netloc, urlParts.path, params, urlParts.fragment))
+		
+		headers = self.headers.copy()
+		if headers: headers.update(headers)
+		
+		if self.__opener: opener = self.__opener
+		else: self.__opener = opener = urllib2.build_opener(*self.handleList)
+		
+		if method.lower() == "get":
+			request = urllib2.Request(url)#, headers if headers else {)
+			response = opener.open(request, timeout=timeout)
+			return RequestRes(response)
+		elif method.lower() == "post":
+			request = urllib2.Request(url, data, headers if headers else None)
+			response = opener.open(request, timeout=timeout)
+			return RequestRes(response)
+	
+	def get(self, url, params=None, **kwargs):
+		return self.request("get", url, params, **kwargs)
+	
+	def post(self, url, data, **kwargs):
+		return self.request("post", url, data=data, **kwargs)
+
+
+class CacheAdapter(urllib2.BaseHandler):
+	def __init__(self, cache_dir, max_age, *args, **kwargs):
+		self._cache_dir = cache_dir
+		self._max_age = max_age
+		self.from_cache = False
+		self._cache = None
+		self._before = None
+	
+	def http_request(self, request):
+		''' Add Accept-Encoding & User-Agent Headers '''
+		request.add_header("Accept-encoding", "gzip, deflate")
+		request.add_header("Accept-language", "en-gb,en-us,en")
+		return request
+	
+	def default_open(self, request):
+		max_age = 0 if refresh is True else int(request.headers.pop("X-Max-Age", self._max_age))
+		self.from_cache = False
+		
+		if request.get_method() == "GET":
+			# Initialize Cache Handler
+			url = request.get_full_url()
+			hash = self._encode_url(url)
+			self._cache = cache = CacheHandler(self._cache_dir, hash, preload_content=True)
+			if cache.exists():
+				if cache.fresh(max_age):
+					# Build the request response and return
+					self.from_cache = True
+					return cache.response(url)
+				else:
+					# Set cache headers to allow for 304 Not Modified response
+					for key, value in cache.conditional_headers.iteritems():
+						request.add_header(key, value)
+		
+		if self.from_cache is False:
+			self._before = time.time()
+	
+	@staticmethod
+	def _encode_url(url):
+		""" Return url as a sha1 encoded hash """
+		if "#" in url: url = url[:url.find("#")]
+		hash = hashlib.sha1(url).hexdigest()
+		return hash
+	
+	def http_response(self, request, response):
+		if self.from_cache: return response
+		elif request.get_method() == "GET":
+			logger.debug("HTTP Request took %s seconds to complete", time.time() - self._before)
+			# Create cache object if for some reason it was not already created
+			if self._cache is None:
+				hash = self._encode_url(request.get_full_url())
+				self._cache = CacheHandler(self._cache_dir, hash, preload_content=False)
+			
+			# Check for a 304 Not Modified Response and use cache if true
+			if response.code == 304:
+				# Refresh the cache as it's still fresh
+				self._cache.update_cache()
+				
+				# Set response to cached response
+				return self._cache.response(request.get_full_url())
+			
+			# Always cache a 301 Moved Permanently response
+			elif response.code == 301:
+				# Cache the response
+				self._cache.cache_response(response)
+			
+			# Cache any cachable response
+			elif response.code in (200, 203, 300):
+				self._cache.cache_response(response)
+				return self._cache.response(request.get_full_url())
+		
+		# Return Response
+		return response
+	
+	# Redirect HTTPS Requests and Responses to HTTP
+	https_request = http_request
+	https_response = http_response
+
+
+class CacheHandler(object):
+	"""
+	Cache Handler to handle the cache thats stored on disk
+	
+	cache_dir : string or unicode --- Directory where to store the cache files
+	max_age : integer --- Max age that the cache can be before it becomes stale (default 0)
+	hash : string or unicode --- sha256 hash of the url of the request to be cached
+	"""
+	
+	def __init__(self, cache_dir, hash, preload_content=False):
+		# Construct the full path to cache
+		self._cache_path = os.path.join(cache_dir, hash)
+		self._response = None
+		
+		# Check if there is a cache for giving url
+		if not os.path.exists(self._cache_path):
+			if not os.path.exists(cache_dir):
+				os.makedirs(cache_dir)
+		
+		# Cache must exist then deserialize the cache
+		elif preload_content is True: self.load()
+	
+	def load(self):
+		""" Load the content of cache into memory """
+		self._response = self.deserialize()
+	
+	@staticmethod
+	def delete(path):
+		""" Delete cache on disk"""
+		try: os.remove(path)
+		except OSError: logger.debug("Cache Error: Unable to delete cache from disk")
+		self.close()
+	
+	@property
+	def response(self):
+		""" Return the cache response as a urllib3 HTTPResponse """
+		if "timestamp" in self._response: del self._response["timestamp"]
+		return functools.partial(HTTPResponse, self._response["body"], self._response["headers"], self._response["status"], self._response["reason"])
+	
+	@property
+	def conditional_headers(self):
+		""" Return a dict of conditional headers from cache """
+		
+		# Fetch Cached headers
+		headers = self._response["headers"]
+		new_headers = {}
+		
+		# Check for conditional headers
+		if "etag" in headers: new_headers["If-None-Match"] = headers["etag"]
+		if "last-modified" in headers: new_headers["If-Modified-Since"] = headers["last-modified"]
+		return new_headers
+	
+	def deserialize(self):
+		""" Return a deserialize version of the cache from disk """
+		
+		try:
+			# Fetch raw cache data
+			with open(self._cache_path, "rb") as stream:
+				raw_data = stream.read()
+		except IOError, OSError:
+			self.delete(self._cache_path)
+			return None
+		else:
+			if not raw_data:
+				self.delete(self._cache_path)
+				return None
+		
+		try:
+			# Deserialize and decode the raw data
+			uncompressed = zlib.decompress(raw_data)
+			cached = json.loads(uncompressed)
+			cached["body"] = base64.b64decode(str(cached["body"]))
+			cached["headers"] = httplib.HTTPMessage(StringIO.StringIO(base64.b64decode(str(cached["headers"]))))
+		except ValueError, TypeError:
+			self.delete(self._cache_path)
+			return None
+		except zlib.error:
+			self.delete(self._cache_path)
+			return None
+		else:
+			return cached
+	
+	def exists(self):
+		""" Return True if cache exists else False """
+		return self._response is not None
+	
+	def fresh(self, max_age):
+		""" Return True if cache is fresh else False """
+		if max_age == 0: return False
+		else: return max_age < 0 or self._response["status"] == 301 or (time.time() - self._response.get("timestamp", 0)) < max_age * 60
+	
+	def update_cache(self, response=None):
+		"""
+		Serialize the response and save to disk
+		
+		[response] : dict --- Dict containging server response data (default None)
+		
+		NOTE
+		If no response is giving then the currently stored response is used
+		"""
+		
+		# Fetch response if not giving one
+		if response is None: response = self._response.copy()
+		
+		# Set the timestamp of the response
+		response["timestamp"] = time.time()
+		
+		# Serialize the response content to insure that json can do it's job
+		response["body"] = base64.b64encode(response["body"])
+		response["headers"] = base64.b64encode(str(response["headers"]))
+		
+		# Serialize the whole response
+		try: jsonSerial = json.dumps(response, ensure_ascii=True, indent=4, separators=(",", ":"))
+		except TypeError:
+			logger.debug("Cache Error: Failed to serialize the response using json")
+			return None
+		
+		# Compress the json Serialized response
+		try: compressed = zlib.compress(jsonSerial, 1)
+		except zlib.error:
+			logger.debug("Cache Error: Failed to compress serialized response")
+			return None
+		
+		# Save serialized response to disk
+		try: 
+			with open(self._cache_path, "wb") as stream:
+				stream.write(compressed)
+		except IOError, OSError:
+			logger.debug("Cache Error: Failed to Save serialized response to disk")
+			self.delete(self._cache_path)
+			return None
+	
+	def cache_response(self, response, body=None):
+		"""
+		Create data structure and Cache the response
+		
+		response : urllib3 response object --- A urllib3 response that was returned from the server
+		[body] : string --- Separated response body to use, uses body from response if body is not giving (default None)
+		"""
+		if body is None: body = response.read()
+		headers = response.info()
+		
+		# Remove Transfer-Encoding from header if response was a chunked response
+		if "transfer-encoding" in headers: del headers["transfer-encoding"]
+		
+		# Create response data structure
+		data = {"body": body,
+				"headers": headers,
+				"reason": response.msg,
+				"status": response.code}
+		
+		# Update cache with response data
+		self._response = data.copy()
+		self.update_cache(data)
+	
+	def close(self):
+		""" Reset instance variables and loaded response"""
+		self._response = None
+
+
+class HTTPResponse(urllib2.addinfourl):
+	def __init__(self, body=None, headers=None, status=None, reason=None, url=None):
+		self.msg = reason
+		urllib2.addinfourl.__init__(self, StringIO.StringIO(body), headers, url, status)
