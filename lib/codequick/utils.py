2b45a3962467eded52b5833cce6af55a37625e10
William Forde <willforde@gmail.com>
2016-11-16 19:58:40 +0000

    Replace SectionParser with ETBuilder

--- lib/codequick/utils.py
+++ lib/codequick/utils.py
@@ -3,6 +3,7 @@ import xbmc
 import xbmcgui
 from .support import logger, get_addon_data
 from HTMLParser import HTMLParser
+from xml.etree import ElementTree as ElementTree
 
 __all__ = ["keyboard", "notification", "get_skin_name", "strip_tags", "requests_session"]
 
@@ -119,83 +120,110 @@ def strip_tags(html):
     return html
 
 
-class SectionParser(HTMLParser):
-    def __init__(self, tag_name=None, attrs=None):
-        """ Initialize and reset this instance. """
+class ETBuilder(HTMLParser):
+    def __init__(self, wanted_tags, tag=None, attrs=None, root_tag=None):
+        """Initialize and reset this instance."""
         HTMLParser.__init__(self)
-        self._search_tag = tag_name
-        self._counter = 0
+        self._in_sec = False if tag else True
+        self._tree = ElementTree.TreeBuilder()
+        self._search_tag = tag
+        self._root_tag = root_tag
+        self._section_elem = None
 
-        # Split attributes into wanted an unwanted attributes
-        self._unwanted_attrs = [attrs.pop(key) for key, value in attrs.items() if value is False]
-        self._wanted_attrs = attrs
+        # Make sure that wanted_tags is a list
+        if isinstance(wanted_tags, basestring):
+            wanted_tags = [wanted_tags]
+        elif isinstance(wanted_tags, tuple):
+            wanted_tags = list(wanted_tags)
+        else:
+            msg = "Wanted tags is not of type: str, unicode, tuple, or list, %s" % type(wanted_tags)
+            assert isinstance(wanted_tags, list), msg
 
-        self.start_pos = None
-        self.end_pos = None
+        # Append the search tag to wanted tags so the parser can use it as the root tag
+        if tag and tag not in wanted_tags:
+            wanted_tags.append(tag)
 
-    def filter(self, html):
-        # Parse the html to find the requested tag section start and end line numbers
+        # Split attributes into wanted and unwanted attributes
+        self._unw_attrs = [attrs.pop(key) for key, value in attrs.items() if value is False] if attrs else []
+        self._w_tags = wanted_tags
+        self._w_attrs = attrs
+
+    def run(self, html):
+        # Set a root tag if givin
+        if self._root_tag:
+            self._tree.start(self._root_tag, {})
+
+        # Parse the document
         try:
             self.feed(html)
         except EOFError:
             pass
 
-        # Filter down to only the lines that are requested
-        filtered_lines = [line for count, line in enumerate(html.split("\n")) if
-                          (self.start_pos[0] - 2) < count < self.end_pos[0]]
+        # End the root tag if set
+        if self._root_tag:
+            self._tree.end(self._root_tag)
 
-        # Strip out any unwanted text in the first and last line
-        filtered_lines[0] = filtered_lines[0][self.start_pos[1]:]
-        filtered_lines[-1] = filtered_lines[-1][:self.end_pos[1] + len("</%s>" % self._search_tag)]
-
-        # Convert the filtered lines back into a unicode string
-        return u"\n".join(filtered_lines)
+        # Close the _tree builder and return the root
+        # tag that is returned by the treebuilder
+        return self._tree.close()
 
     def handle_starttag(self, tag, attrs):
-        if tag == self._search_tag:
-            # Increment the tag counter to indicate that we are still inside the requested tag
-            # When tag counter returns back to zero then we must be finished with that tag section
-            if self._counter > 0:
-                self._counter += 1
+        # Search for requested section if required
+        if self._in_sec is False and tag == self._search_tag and self._check_attrs(attrs):
+            elem = self._tree.start(tag, dict(attrs) if attrs else {})
+            self._in_sec = True
+            self._section_elem = elem
 
-            # If we have required attrs to match then search all attrs for wanted attrs
-            # And also check that we do not have any attrs that are unwanted
-            elif attrs and (self._wanted_attrs or self._unwanted_attrs):
-                wanted_attrs = self._wanted_attrs.copy()
-                unwanted_attrs = self._unwanted_attrs
-                for key, value in attrs:
-                    # Check for unwanted attrs
-                    if key in unwanted_attrs:
-                        return None
+        # Build tree of requested element tags
+        elif self._in_sec and (not self._w_tags or tag in self._w_tags):
+            self._tree.start(tag, dict(attrs) if attrs else {})
 
-                    # Check for wanted attrs
-                    elif key in wanted_attrs:
-                        c_value = wanted_attrs[key]
-                        if c_value == value or c_value is True:
-                            # Remove the attra form the wanted dict of attrs
-                            # Indicates that this attr has been found
-                            del wanted_attrs[key]
+    def _check_attrs(self, attrs):
+        # If we have required attrs to match then search all attrs for wanted attrs
+        # And also check that we do not have any attrs that are unwanted
+        if attrs and (self._w_attrs or self._unw_attrs):
+            wanted_attrs = self._w_attrs.copy()
+            unwanted_attrs = self._unw_attrs
+            for key, value in attrs:
+                # Check for unwanted attrs
+                if key in unwanted_attrs:
+                    return False
 
-                # Check that wanted_attrs is empty sense empty means that all attrs ware found
-                # Now the tag counter can start so to indicate that we are inside the requested tag
-                if not wanted_attrs:
-                    self._counter = 1
-                    self.start_pos = self.getpos()
+                # Check for wanted attrs
+                elif key in wanted_attrs:
+                    c_value = wanted_attrs[key]
+                    if c_value == value or c_value is True:
+                        # Remove the attra form the wanted dict of attrs
+                        # Indicates that this attr has been found
+                        del wanted_attrs[key]
 
-            # Start tag counter if requested tag is found and there is no attributes required
-            elif not self._wanted_attrs:
-                self._counter = 1
-                self.start_pos = self.getpos()
+            # Check that wanted_attrs is empty sense empty means that all attrs ware found
+            # Now the tag counter can start_time so to indicate that we are inside the requested tag
+            if not wanted_attrs:
+                return True
+
+        # Start tag counter if requested tag is found and there is no attributes required
+        elif not self._w_attrs:
+            return True
 
     def handle_endtag(self, tag):
-        if tag == self._search_tag:
-            # When tag counter is already at 1 then we must be at the requested end tag so we must
-            # raise EOFError to stop parsing anymore html
-            if self._counter == 1:
-                self.end_pos = self.getpos()
-                raise EOFError
+        # Close current tree element for requested tags
+        if self._in_sec and (not self._w_tags or tag in self._w_tags) and self._section_elem is self._close_tag(tag):
+            self._in_sec = False
+            raise EOFError
 
-            # We must not yet be at the end of the requested tag
-            # So just decrement the tag counter
-            else:
-                self._counter -= 1
+    def handle_data(self, data):
+        # Add tag data to element only if it contains text and not just an empty string with white spaces
+        data = data.strip()
+        if data and self._in_sec and (not self._w_tags or self.lasttag in self._w_tags):
+            self._tree.data(data)
+
+    def _close_tag(self, tag):
+        try:
+            # Close the current tree element
+            return self._tree.end(tag)
+        except AssertionError:
+            # Attemp to fix tree if a bad end tag was detected
+            print "Bad end tag detected", tag, self.lasttag
+            self._tree.end(self.lasttag)
+            return self._tree.end(tag)
