40452f769c1fe7a0115f1555adb86c7edb10b86d
William Forde <willforde@gmail.com>
2017-06-16 22:42:20 +0100

    change format of license

--- lib/codequick/__init__.py
+++ lib/codequick/__init__.py
@@ -1,20 +1,19 @@
-"""
-Copyright: (c) 2013 - 2016 William Forde (willforde+codequick@gmail.com)
-License: GPLv3, see LICENSE for more details
+# Copyright: (c) 2016 - 2017 William Forde (willforde+codequick@gmail.com)
+# License: GPLv3, see LICENSE for more details
+#
+# codequick is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# codequick is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-codequick is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-codequick is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <http://www.gnu.org/licenses/>.
-"""
 # Standard Library Imports
 import time
 
