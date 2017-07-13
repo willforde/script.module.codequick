f4242ab7a8b4c78fb72255de52a4ec89b387b70c
William Forde <willforde@gmail.com>
2017-07-13 04:52:38 +0100

    more reordering

--- lib/codequick/utils.py
+++ lib/codequick/utils.py
@@ -89,26 +89,6 @@ class Params(dict):
                     self.callback_params[key] = value
 
 
-def parse_qs(qs):
-    """
-    Parse a urlencoded query string, and return the data as a dictionary.
-
-    :param qs: Percent-encoded query string to be parsed.
-    :type qs: str, unicode
-
-    :return: Returns a dict of key/value pairs with the value as unicode.
-    :rtype: dict
-    """
-    params = {}
-    for key, value in urlparse.parse_qsl(qs.encode("utf8") if isinstance(qs, unicode) else qs):
-        if key not in params:
-            params[key] = unicode(value, encoding="utf8")
-        else:
-            raise ValueError("encountered duplicate param field name: '{}'".format(key))
-
-    return params
-
-
 def parse_sysargs():
     """
     Extract calling arguments from system arguments.
@@ -167,3 +147,23 @@ def keyboard(heading, default="", hidden=False):
         return unicode(text, "utf8")
     else:
         return u""
+
+
+def parse_qs(qs):
+    """
+    Parse a urlencoded query string, and return the data as a dictionary.
+
+    :param qs: Percent-encoded query string to be parsed.
+    :type qs: str, unicode
+
+    :return: Returns a dict of key/value pairs with the value as unicode.
+    :rtype: dict
+    """
+    params = {}
+    for key, value in urlparse.parse_qsl(qs.encode("utf8") if isinstance(qs, unicode) else qs):
+        if key not in params:
+            params[key] = unicode(value, encoding="utf8")
+        else:
+            raise ValueError("encountered duplicate param field name: '{}'".format(key))
+
+    return params
