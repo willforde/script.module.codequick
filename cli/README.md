5ef1aa63a4ea1b5b40497b033d2b89c0e609881d
William Forde <willforde@gmail.com>
2017-09-17 02:51:39 +0100

    move cli components to its own package

--- cli/README.md
+++
@@ -1,4 +0,0 @@
-# codequick-cli
-Launch kodi add-ons from outside kodi. WIP
-
-Documentation to come.
--- template/create.py
+++
@@ -1,3 +0,0 @@
-# This will be a simple script to create codequick templates
-# Templates will be customized based on user input
-pass
