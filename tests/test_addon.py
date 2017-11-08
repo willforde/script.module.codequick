d1a94eb241162d6493180cb14e1bfa5e0ca8977f
William Forde <willforde@gmail.com>
2017-11-08 02:59:32 +0000

    initial unittest

---
+++ tests/test_addon.py
@@ -0,0 +1,18 @@
+from addondev import initializer
+import os
+
+initializer(os.path.join(os.path.dirname(os.path.dirname(__file__)), "script.module.codequick"))
+import unittest
+from codequick import utils
+
+
+class Utils(unittest.TestCase):
+    def test_ensure_unicode_with_bytes(self):
+        ret = utils.ensure_unicode(b"teststring")
+        self.assertIsInstance(ret, utils.unicode_type)
+        self.assertEqual(ret, u"teststring")
+
+    def test_ensure_unicode_with_unicode(self):
+        ret = utils.ensure_unicode(u"teststring")
+        self.assertIsInstance(ret, utils.unicode_type)
+        self.assertEqual(ret, u"teststring")
