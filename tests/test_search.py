588a8a85daacb18e1359b5d135dd57a9d0404aed
William Forde <willforde@gmail.com>
2018-02-27 21:17:17 +0000

    update search unittest to match new changes

--- tests/test_search.py
+++ tests/test_search.py
@@ -6,6 +6,7 @@ import os
 # Testing specific imports
 from codequick import search, route, storage
 from codequick.support import dispatcher
+from codequick.listing import Listitem
 
 
 class TestGlobalLocalization(unittest.TestCase):
@@ -34,47 +35,172 @@ class Search(unittest.TestCase):
         dispatcher.registered_routes.clear()
         dispatcher.registered_routes.update(self.org_routes)
 
-    def test_first_run(self):
+    def test_first_load(self):
         @route.Route.register
-        def search_results(_):
-            pass
+        def results(_, search_query):
+            self.assertEqual(search_query, "Rock")
+            yield Listitem.from_dict("listitem one", results)
+            yield Listitem.from_dict("listitem two", results)
 
         with testing.mock_keyboard("Rock"):
-            listitems = search.SavedSearches.test(route=search_results.route.path)
+            listitems = search.SavedSearches.test(first_load=True, route=results.route.path, execute_delayed=True)
+
+        with storage.PersistentList(search.SEARCH_DB) as db:
+            self.assertIn("Rock", db)
 
         self.assertEqual(len(listitems), 2)
-        self.assertEqual(listitems[-1].label, "Rock")
+        self.assertEqual(listitems[0].label, "listitem one")
+        self.assertEqual(listitems[1].label, "listitem two")
 
-    def test_force_search(self):
+    def test_first_load_invalid(self):
         @route.Route.register
-        def search_results(_):
-            pass
+        def results(_, search_query):
+            self.assertEqual(search_query, "Rock")
+            return False
 
         with testing.mock_keyboard("Rock"):
-            listitems = search.SavedSearches.test(search=True, route=search_results.route.path)
+            listitems = search.SavedSearches.test(first_load=True, route=results.route.path, execute_delayed=True)
 
-        self.assertEqual(len(listitems), 2)
-        self.assertEqual(listitems[-1].label, "Rock")
-
-    def test_empty_search(self):
-        @route.Route.register
-        def search_results(_):
-            pass
-
-        with testing.mock_keyboard(""):
-            listitems = search.SavedSearches.test(route=search_results.route.path)
+        with storage.PersistentList(search.SEARCH_DB) as db:
+            self.assertNotIn("Rock", db)
 
         self.assertFalse(listitems)
 
-    def test_remove(self):
+    def test_first_load_canceled(self):
+        # noinspection PyUnusedLocal
         @route.Route.register
-        def search_results(_):
+        def results(_, search_query):
             pass
 
-        with storage.PersistentList(u"_searches.pickle") as db:
-            db.append("Rock")
+        with testing.mock_keyboard(""):
+            listitems = search.SavedSearches.test(first_load=True, route=results.route.path, execute_delayed=True)
+
+        with storage.PersistentList(search.SEARCH_DB) as db:
+            self.assertFalse(bool(db))
+
+        self.assertFalse(listitems)
+
+    def test_search_empty(self):
+        @route.Route.register
+        def results(_, search_query):
+            self.assertEqual(search_query, "Rock")
+            yield Listitem.from_dict("listitem one", results)
+            yield Listitem.from_dict("listitem two", results)
+
+        with testing.mock_keyboard("Rock"):
+            listitems = search.SavedSearches.test(search=True, route=results.route.path, execute_delayed=True)
+
+        with storage.PersistentList(search.SEARCH_DB) as db:
+            self.assertIn("Rock", db)
+
+        self.assertEqual(len(listitems), 2)
+        self.assertEqual(listitems[0].label, "listitem one")
+        self.assertEqual(listitems[1].label, "listitem two")
+
+    def test_search_populated(self):
+        @route.Route.register
+        def results(_, search_query):
+            self.assertEqual(search_query, "Rock")
+            yield Listitem.from_dict("listitem one", results)
+            yield Listitem.from_dict("listitem two", results)
+
+        with storage.PersistentList(search.SEARCH_DB) as db:
+            db.append("Pop")
             db.flush()
 
-        listitems = search.SavedSearches.test("Rock", route=search_results.route.path)
-        self.assertEqual(len(listitems), 1)
-        self.assertNotEqual(listitems[0].label, "rock")
+        with testing.mock_keyboard("Rock"):
+            listitems = search.SavedSearches.test(search=True, route=results.route.path, execute_delayed=True)
+
+        with storage.PersistentList(search.SEARCH_DB) as db:
+            self.assertIn("Rock", db)
+            self.assertIn("Pop", db)
+
+        self.assertEqual(len(listitems), 2)
+        self.assertEqual(listitems[0].label, "listitem one")
+        self.assertEqual(listitems[1].label, "listitem two")
+
+    def test_search_populated_invalid(self):
+        # noinspection PyUnusedLocal
+        @route.Route.register
+        def results(_, search_query):
+            pass
+
+        with storage.PersistentList(search.SEARCH_DB) as db:
+            db.append("Pop")
+            db.flush()
+
+        with testing.mock_keyboard(""):
+            listitems = search.SavedSearches.test(search=True, route=results.route.path, execute_delayed=True)
+
+        with storage.PersistentList(search.SEARCH_DB) as db:
+            self.assertIn("Pop", db)
+
+        self.assertEqual(len(listitems), 2)
+        self.assertIn("Search", listitems[0].label)
+        self.assertEqual(listitems[1].label, "Pop")
+
+    def test_saved_firstload(self):
+        # noinspection PyUnusedLocal
+        @route.Route.register
+        def results(_, search_query):
+            pass
+
+        with storage.PersistentList(search.SEARCH_DB) as db:
+            db.append("Rock")
+            db.append("Pop")
+            db.flush()
+
+        listitems = search.SavedSearches.test(first_load=True, route=results.route.path, execute_delayed=True)
+
+        with storage.PersistentList(search.SEARCH_DB) as db:
+            self.assertIn("Rock", db)
+            self.assertIn("Pop", db)
+
+        self.assertEqual(len(listitems), 3)
+        self.assertIn("Search", listitems[0].label)
+        self.assertEqual(listitems[1].label, "Rock")
+        self.assertEqual(listitems[2].label, "Pop")
+
+    def test_saved_not_firstload(self):
+        # noinspection PyUnusedLocal
+        @route.Route.register
+        def results(_, search_query):
+            pass
+
+        with storage.PersistentList(search.SEARCH_DB) as db:
+            db.append("Rock")
+            db.append("Pop")
+            db.flush()
+
+        with testing.mock_keyboard("Rock"):
+            listitems = search.SavedSearches.test(route=results.route.path, execute_delayed=True)
+
+        with storage.PersistentList(search.SEARCH_DB) as db:
+            self.assertIn("Rock", db)
+            self.assertIn("Pop", db)
+
+        self.assertEqual(len(listitems), 3)
+        self.assertIn("Search", listitems[0].label)
+        self.assertEqual(listitems[1].label, "Rock")
+        self.assertEqual(listitems[2].label, "Pop")
+
+    def test_saved_remove(self):
+        # noinspection PyUnusedLocal
+        @route.Route.register
+        def results(_, search_query):
+            pass
+
+        with storage.PersistentList(search.SEARCH_DB) as db:
+            db.append("Rock")
+            db.append("Pop")
+            db.flush()
+
+        listitems = search.SavedSearches.test(remove_entry="Rock", route=results.route.path, execute_delayed=True)
+
+        with storage.PersistentList(search.SEARCH_DB) as db:
+            self.assertNotIn("Rock", db)
+            self.assertIn("Pop", db)
+
+        self.assertEqual(len(listitems), 2)
+        self.assertIn("Search", listitems[0].label)
+        self.assertEqual(listitems[1].label, "Pop")
