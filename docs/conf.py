72b8e6debbc6a98ff0a59656d52d6e1d12175d4f
William Forde <willforde@gmail.com>
2018-03-07 18:45:44 +0000

    test read the docs pdf

--- docs/conf.py
+++ docs/conf.py
@@ -137,8 +137,7 @@ latex_elements = {
     # Latex figure (float) alignment
     #
     # 'figure_align': 'htbp',
-    'classoptions': ',openany,oneside',
-    'babel' : '\\usepackage[polish]{babel}'
+    'classoptions': ',openany,oneside'
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
