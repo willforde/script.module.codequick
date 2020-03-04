fc9e354a5707f6b376e8fba4d97dbeea9ce56d75
willforde <willforde@gmail.com>
2020-03-04 16:11:17 +0000

    fix for issue #32

--- script.module.codequick/lib/urlquick.py
+++ script.module.codequick/lib/urlquick.py
@@ -1561,7 +1561,7 @@ def auto_cache_cleanup(max_age=60 * 60 * 24 * 14):
 #############
 
 # Set the location of the cache file to the addon data directory
-_addon_data = __import__("xbmcaddon").Addon("plugin.video.science.friday")
+_addon_data = __import__("xbmcaddon").Addon()
 _CACHE_LOCATION = __import__("xbmc").translatePath(_addon_data.getAddonInfo("profile"))
 CACHE_LOCATION = _CACHE_LOCATION.decode("utf8") if isinstance(_CACHE_LOCATION, bytes) else _CACHE_LOCATION
 logger.debug("Cache location: %s", CACHE_LOCATION)
